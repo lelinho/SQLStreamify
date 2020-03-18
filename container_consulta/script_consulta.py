c460ccd41d22d6254f1921abc7b1453d74fd27cd
Aurelio Carlos <lelinhoprado@gmail.com>
2020-03-18 17:14:54 -0300

    dia 18032020

--- container_consulta/script_consulta.py
+++ container_consulta/script_consulta.py
@@ -1,24 +1,18 @@
 import pymysql as MySQLdb
-import time
 
 # Open database connection
-db = MySQLdb.connect("mysql","zabbix","z@bb1x","zabbix" )
+db = MySQLdb.connect("172.17.0.1","zabbix","z@bb1x","zabbix" )
 
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
 
-inicio = time.time()
-cursor.execute("SELECT `employees`.`first_name`, `employees`.`last_name`, `salaries`.`from_date`, `salaries`.`to_date`, `salaries`.`salary` FROM `employees` LEFT JOIN `salaries` ON `employees`.`emp_no` = `salaries`.`emp_no` ORDER BY `salary` DESC LIMIT 10")
 
+cursor.execute("SELECT * FROM history_uint WHERE itemid=64431 ORDER BY clock DESC limit 30")
 data = cursor.fetchall()
-fim = time.time()
-
 
 for row in data:
-    print(row[0], row[1]," - ", row[4])
+    print(row[0])
 
-
-print(f"Tempo de execucao da consulta: {(fim - inicio)}")
 
 # disconnect from server
 db.close()
