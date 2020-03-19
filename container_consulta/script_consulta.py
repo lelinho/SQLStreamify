0defb2029d9245fec7637d62f20f5d11d3aec820
Aurelio Carlos <lelinhoprado@gmail.com>
2020-03-19 17:06:20 -0300

    dia 19032020

--- container_consulta/script_consulta.py
+++ container_consulta/script_consulta.py
@@ -1,4 +1,5 @@
 import pymysql as MySQLdb
+from prettytable import from_db_cursor
 
 # Open database connection
 db = MySQLdb.connect("172.17.0.1","zabbix","z@bb1x","zabbix" )
@@ -6,13 +7,10 @@ db = MySQLdb.connect("172.17.0.1","zabbix","z@bb1x","zabbix" )
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
 
+cursor.execute("SELECT * FROM history_uint WHERE itemid=27456 ORDER BY clock DESC limit 5")
+data = from_db_cursor(cursor)
 
-cursor.execute("SELECT * FROM history_uint WHERE itemid=64431 ORDER BY clock DESC limit 30")
-data = cursor.fetchall()
-
-for row in data:
-    print(row[0])
-
+print(data)
 
 # disconnect from server
 db.close()
