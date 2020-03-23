b1dc9ec68dafb2e6c604af1562c3be648d8b995a
lelinhoprado <lelinhoprado@gmail.com>
2020-03-23 15:46:05 -0300

    tarde 23032020

--- container_consulta/script_consulta.py
+++ container_consulta/script_consulta.py
@@ -1,26 +1,74 @@
 import pymysql as MySQLdb
-from prettytable import from_db_cursor
+import configparser
+import socket
+import pandas as pd
+import re
+from pandas.io import sql
 from flask import Flask, escape, request
 
-# Open database connection
-db = MySQLdb.connect("172.17.0.1","zabbix","z@bb1x","zabbix" )
+# Le informaçoes do arquivo de configuracao
+config = configparser.ConfigParser()
+config.read('config.ini')
 
-# prepare a cursor object using cursor() method
-cursor = db.cursor()
+#consulta = config['SQL']['query']
+consulta = "Select a.col1, b.col2 from tb1 as a inner join tb2 as b on tb1.col7 = tb2.col8;"
 
-cursor.execute("SELECT * FROM history_text WHERE itemid=59197 ORDER BY clock DESC limit 5")
-data = from_db_cursor(cursor)
+app = Flask(__name__)    
+hostname = socket.gethostname()
 
-# disconnect from server
-db.close()
-
-app = Flask(__name__)
-@app.route('/')
-def print():
-    return f'{escape(data)}'
+@app.route("/")
+def index():
+    return "script_consulta running on {}\n".format(hostname)
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+@app.route("/<string:tabela>")
+def query(tabela):
+    # Conexão com o Banco de Dados
+    db = MySQLdb.connect(config['DB']['host'],config['DB']['user'],config['DB']['password'],config['DB']['db'])
+    
+    # prepara o cursor
+    #cursor = db.cursor()
+    e = pd.read_sql(config['SQL']['query'],db)
+    b = e.to_json()
+    
+    #cursor.execute(config['SQL']['query'])
+    #data = cursor.fetchall()
+    #result = dict(data)
+
+    # disconnect from server
+    db.close()
+    return f'{escape(b)}'
+
+@app.route("/sql/<string:sql_str>")
+def tables_in_query(sql_str):
+    sql_str = consulta
+    # remove the /* */ comments
+    q = re.sub(r"/\*[^*]*\*+(?:[^*/][^*]*\*+)*/", "", sql_str)
+
+    # remove whole line -- and # comments
+    lines = [line for line in q.splitlines() if not re.match("^\s*(--|#)", line)]
+
+    # remove trailing -- and # comments
+    q = " ".join([re.split("--|#", line)[0] for line in lines])
+
+    # split on blanks, parens and semicolons
+    tokens = re.split(r"[\s)(;]+", q)
+
+    # scan the tokens. if we see a FROM or JOIN, we set the get_next
+    # flag, and grab the next one (unless it's SELECT).
+
+    table = set()
+    get_next = False
+    for tok in tokens:
+        if get_next:
+            if tok.lower() not in ["", "select"]:
+                print(tok)
+                table.add(tok)
+            get_next = False
+        get_next = tok.lower() in ["from", "join"]
+    
+    return f'{escape(str(table))}'
 
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
