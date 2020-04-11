e6ed506c5676e12770118ba469d6547ec1b92bcf
lelinhoprado <lelinhoprado@gmail.com>
2020-04-11 17:35:08 -0300

    - alterado Flask para uso do uWSGI ao invés do servidor de testes

--- webui/webui.py
+++ webui/webui.py
@@ -62,4 +62,5 @@ def index():
 if __name__ == "__main__":
     app.jinja_env.auto_reload = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run(debug=True, host='0.0.0.0', port=80)
+    #app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(host="0.0.0.0", port=80)
